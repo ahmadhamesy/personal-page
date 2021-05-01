d67d1ca40dd70098baa9656ee677d75df11a6ced
ahmadecommerce <ahmadecommerce@gmail.com>
2021-05-01 06:58:59 +0300

    personal page

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$(window).scroll(function(){
+    if($(this).scrollTop()>=200){
+        $("#navBar").addClass("noTransperrent");
+    }
+    else{
+        $("#navBar").removeClass("noTransperrent");
+    }
+});
+$('.circle').circleProgress({
+    startAngle: -Math.PI / 2,
+    fill: "#0575e6"
+}).on('circle-animation-progress',function(event, progress ,stepValue){
+    $(this).find('span').html(Math.round(stepValue * 100)+ '%' );
+
+});
+$(function () {
+    $("#commentForm").validate();
+ });
